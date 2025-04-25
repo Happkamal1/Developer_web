c7c33c40a636a57cc8be60ba688ec8db6b2dffd7
Kamal Gupta <kg9118545@gmail.com>
2025-04-25 16:09:19 +0530

    Update GuideToHire.jsx

--- src/components/GuideToHire.jsx
+++ src/components/GuideToHire.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Coder from "../images/GuideToHire/coder.png";
-import True from "../images/GuideToHire/True.png";
+import True from "../images/GuideToHire/true.png";
 
 const GuideToHire = () => {
   return (
