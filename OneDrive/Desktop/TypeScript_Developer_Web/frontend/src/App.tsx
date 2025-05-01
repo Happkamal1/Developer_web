cf01d2ae9e0d1067616c9118bd0abb6cf0b2e480
Happkamal1 <Kamal Gupta>
2025-05-01 14:55:47 +0530

    Converted JSX to TSX

---
+++ OneDrive/Desktop/TypeScript_Developer_Web/frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Home from "./Pages/Home";
+// import Header from "./components/Header";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <Home />
+    </>
+  );
+};
+
+export default App;
